713b0f5e13cf880d3d3d08bdffe016b681849f85
dotslash21 <arunangshubsws@gmail.com>
2019-01-02 16:59:37 +0530

    Firebase integration compplete
    Signed-off-by: dotslash21 <arunangshubsws@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAyDrz6DxrjCeYNi3KFRk-nhcAOvDhQ5MY",
+    authDomain: "oshop-ada64.firebaseapp.com",
+    databaseURL: "https://oshop-ada64.firebaseio.com",
+    projectId: "oshop-ada64",
+    storageBucket: "oshop-ada64.appspot.com",
+    messagingSenderId: "544718130415"
+  }
 };
