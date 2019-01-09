ebce7751ecd7fbcdab7d6898d1d14b0582b73f70
dotslash21 <arunangshubsws@gmail.com>
2019-01-09 17:35:35 +0530

    Implemented order placing mechanism
    Signed-off-by: dotslash21 <arunangshubsws@gmail.com>

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ShoppingCartService } from "../shopping-cart.service";
+import { ShoppingCart } from "../models/shopping-cart";
+import { Subscription } from "rxjs/Subscription";
+import { OrderService } from "../order.service";
 
 @Component({
-  selector: 'app-check-out',
-  templateUrl: './check-out.component.html',
-  styleUrls: ['./check-out.component.css']
+  selector: "app-check-out",
+  templateUrl: "./check-out.component.html",
+  styleUrls: ["./check-out.component.css"]
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+  shipping = {};
+  cart: ShoppingCart;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private orderService: OrderService
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.subscribe(cart => (this.cart = cart));
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  placeOrder() {
+    let order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(item => {
+        return {
+          product: {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            price: item.price
+          },
+          quantity: item.quantity,
+          totalPrice: item.totalPrice
+        };
+      })
+    };
+
+    this.orderService.storeOrder(order);
+  }
 }
